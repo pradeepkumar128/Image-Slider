83fa6e3c89a742c56d763743834a0c33ecebec13
pradeepkumar128 <iampradeep1282@gmail.com>
2024-05-16 23:50:15 +0530

    slider project done

---
+++ App.js
@@ -0,0 +1,17 @@
+const sliderImg = document.querySelector("#sliderImg");
+// const nextButton = document.querySelector('#nextbtn');
+// const prevButton = document.querySelector('#prevbtn');
+
+const images = ["a.jpg", "b.jpg", "c.jpg", "d.jpg", "e.jpg", "f.jpg"];
+
+let imgIndex = 0;
+const sliderInterval = setInterval(() => {
+  if (imgIndex >= images.length) {
+    imgIndex = 0;
+  }
+  const imgUrl = images[imgIndex];
+  sliderImg.setAttribute("src", imgUrl);
+  imgIndex++;
+}, 1000);
+
+
